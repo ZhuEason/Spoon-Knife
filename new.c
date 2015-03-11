4e749afe91f30a6e4d3ee00c70d73683ffbf72ba
Eason <zhuyisong1994@gmail.com>
2015-03-11 16:18:05 +0800

    try new

---
+++ new.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: new.c
+	> Author: zhuyisong
+	> Mail: zhuyisong1994@gmail.com
+	> Created Time: Wed 11 Mar 2015 15:39:30 HKT
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	return 0;
+}
